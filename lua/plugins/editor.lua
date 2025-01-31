2585a33e26f050bc5b56eef779bdf0ae49c60117
rj <rajibbajracharya@gmail.com>
2025-01-31 10:40:48 +0545

    ups spectre

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -129,4 +129,19 @@ return {
     },
     config = function() require("codeium").setup {} end,
   },
+  {
+    "nvim-pack/nvim-spectre",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("spectre").setup()
+    end,
+    keys = {
+      { "<leader>sr", '<cmd>lua require("spectre").toggle()<CR>', desc = "Toggle Spectre" },
+      { "<leader>sw", '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', desc = "Search current word" },
+      { "<leader>sp", '<cmd>lua require("spectre").open_file_search({select_word=true})<CR>', desc = "Search in file" },
+    },
+  }
 }
