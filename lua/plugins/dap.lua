165969cb4443f433fb6137da7ebac8a3e9a4e92a
SeyTo <rajibbajracharya@gmail.com>
2025-12-09 14:21:44 +0545

    react dap added/not tested

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -28,6 +28,16 @@ return {
       },
     }
 
+    dap.adapters["pwa-chrome"] = {
+      type = "server",
+      host = "localhost",
+      port = 9222,
+      executable = {
+        command = "js-debug-adapter",
+        args = { "9222" },
+      },
+    }
+
     -- 2) Shared configuration for TS + JS
     local shared = {
       {
@@ -47,16 +57,55 @@ return {
       },
     }
 
+    local function pick_url(callback)
+      vim.ui.input(
+        { prompt = "URL to debug (default: http://localhost:9000): " },
+        function(input) callback(input ~= "" and input or "http://localhost:9000") end
+      )
+    end
+
+    dap.configurations.javascriptreact = {
+      {
+        name = "Debug Vite React App",
+        type = "pwa-chrome",
+        request = "launch",
+        url = function()
+          return coroutine.create(function(coro)
+            pick_url(function(result) coroutine.resume(coro, result) end)
+          end)
+        end,
+        webRoot = "${workspaceFolder}",
+        runtimeArgs = {
+          "--remote-debugging-port=9222",
+        },
+        sourceMaps = true,
+        trace = true,
+      },
+      {
+        name = "Attach to Chrome (Vite)",
+        type = "pwa-chrome",
+        request = "attach",
+        program = "${file}",
+        cwd = "${workspaceFolder}",
+        port = 9222,
+        webRoot = "${workspaceFolder}",
+        url = function()
+          return coroutine.create(function(coro)
+            pick_url(function(result) coroutine.resume(coro, result) end)
+          end)
+        end,
+      },
+    }
+
+    dap.configurations.typescriptreact = dap.configurations.javascriptreact
+
     -- 3) Attach the configuration to filetypes
     dap.configurations.javascript = shared
     dap.configurations.typescript = shared
   end,
-
-  
-  
 }
 
--- 
+--
 -- {
 --   "compilerOptions": {
 --     "rootDir": "./ts",
